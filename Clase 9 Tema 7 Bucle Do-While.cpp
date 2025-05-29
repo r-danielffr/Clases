20fb81daac530f1887e43e390dee2871decaa8f4
r-danielffr <00246325@uca.edu.sv>
2025-05-29 11:41:04 -0600

    Clases de Fundamentos de Programacion 29/5/2025

---
+++ Clase 9 Tema 7 Bucle Do-While.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+int main()
+{   int numero = 20;
+
+    cout<<"El viernes 20 de Julio es asueto. "<<endl;
+    cout<<" Vamos a imprimir numeros "<<endl;
+        
+    do{
+        cout<<" Imprimir enteros "<<numero<<endl;
+        cout << endl;
+        numero++;
+    }   while (numero <=15);
+    
+    
+    
+return 0;}
